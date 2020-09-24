ecf66d99e383e74c2da1537da69a084c27052e98
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-24 19:44:17 -0400

    review of class components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react' //import react bc react is needed any t
 //we import component bc Component is a class that is the parent class for any react custom component
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0,
+            ...props.state
+        }
+        this.counterUp = this.counterUp.bind(this)
+        this.counterDown = this.counterDown.bind(this)
+        this.counterReset = this.counterReset.bind(this)
 
+        counterUp() {
+            this.setState((prevState) => {
+                return {
+                    ...prevState,
+                    count: prevState.count + 1
+                }
+            })
+        }
+
+        counterDown() {
+            this.state.count = this.state.count - 1
+
+        }
+
+        this.counterReset() {
+            this.state.count = 0
+        }
+    }
     render() {
         return (
             <div>
-                
+                <button onClick={this.counterDown}>-</button>
+                {this.state.count}
+                <button onClick={this.counterUp}>+</button>
+
             </div>
         )
     }
